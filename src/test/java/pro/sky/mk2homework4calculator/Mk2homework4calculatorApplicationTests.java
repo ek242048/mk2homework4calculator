90a6e2fc81b8c6e5dbad9a0a305cbdf14ce26375
Kate <ek242048@yandex.ru>
2022-05-30 20:33:31 +0500

    Initial commit

---
+++ src/test/java/pro/sky/mk2homework4calculator/Mk2homework4calculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.mk2homework4calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mk2homework4calculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
